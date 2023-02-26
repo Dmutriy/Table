fae1d858fca2bb55e35e42d2f9bdd8b6003c5e1a
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-02-26 17:43:16 +0200

    add button save and add new

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,70 +1,103 @@
-import { useParams } from "react-router-dom";
-import React, { useState } from "react";
-
+// import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import css from "./ProductDetails.module.css";
 
 import { getProductById } from "../fakeAPI";
 
 export const ProductDetails = ({ data }) => {
-  console.log(data);
+  // console.log(data);
+  // const { id } = useParams();
+  // const product = getProductById(id);
+
+  // Створюємо стан для збереження даних таблиці
   const [tableData, setTableData] = useState(data);
-  const { id } = useParams();
-  const product = getProductById(id);
+
+  // Функція для зміни значення комірки таблиці
   const handleCellChange = (
     rowIndex,
     columnIndex,
     value
   ) => {
-    // Створюємо новий масив для даних таблиці зі зміненим значенням комірки
+    // Створюємо копію даних таблиці
     const newData = [...tableData];
+    // Замінюємо значення відповідної комірки
     newData[rowIndex][columnIndex] = value;
+    // Встановлюємо змінені дані до стану таблиці
     setTableData(newData);
   };
 
-  return (
-    <main>
-      <img
-        src="https://via.placeholder.com/960x240"
-        alt=""
-      />
-      <div>
-        <h2>
-          Product - {product.name} - {id}
-        </h2>
+  // Функція для додавання нового рядка до таблиці
+  const handleAddRow = () => {
+    // Створюємо новий рядок з порожніми значеннями
+    const newRow = ["", "", "", ""];
+    // Додаємо новий рядок до даних таблиці
+    setTableData([...tableData, newRow]);
+  };
 
-        <table>
-          <thead>
-            <tr>
-              <th>Ім'я</th>
-              <th>Прізвище</th>
-              <th>Вік</th>
-              <th>Електронна пошта</th>
+  // Функція для збереження даних таблиці в локальне сховище
+  const handleSave = () => {
+    // Зберігаємо дані таблиці в локальне сховище
+    localStorage.setItem(
+      "tableData",
+      JSON.stringify(tableData)
+    );
+    // Показуємо повідомлення про успішне збереження
+    alert("Дані успішно збережено!");
+  };
+
+  const loadData = () => {
+    const savedData = localStorage.getItem("tableData");
+    if (savedData) {
+      setTableData(JSON.parse(savedData));
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      {/* Додаємо кнопку для додавання нового рядка */}
+      <button onClick={handleAddRow}>Додати рядок</button>
+      {/* Додаємо кнопку для збереження даних таблиці */}
+      <button onClick={handleSave}>Зберегти</button>
+      <table>
+        <thead>
+          <tr>
+            <th>N</th>
+            <th>Ім'я</th>
+            <th>Прізвище</th>
+            <th>Вік</th>
+            <th>Електронна пошта</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Виводимо дані таблиці */}
+          {tableData.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {/* Додаємо колонку для номера рядка */}
+              <td>{rowIndex + 1}</td>
+              {row.map((cell, columnIndex) => (
+                <td
+                  key={columnIndex}
+                  contentEditable
+                  onBlur={(event) =>
+                    handleCellChange(
+                      rowIndex,
+                      columnIndex,
+                      event.target.innerText
+                    )
+                  }
+                >
+                  {cell}
+                </td>
+              ))}
             </tr>
-          </thead>
-          <tbody>
-            {tableData.map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                {row.map((cell, columnIndex) => (
-                  <td
-                    key={columnIndex}
-                    contentEditable
-                    onBlur={(event) =>
-                      handleCellChange(
-                        rowIndex,
-                        columnIndex,
-                        event.target.innerText
-                      )
-                    }
-                  >
-                    {cell}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </main>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
