be00160f26976b7aad56519c9830b10a8d0981fb
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-02-26 16:43:24 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,148 @@ import { Team } from "./Team";
 import { Reviews } from "./Reviews";
 import { SharedLayout } from "./SharedLayout";
 
+// const data = [
+//   {
+//     name: "Марія",
+//     firstname: "Іванова",
+//     age: 25,
+//     email: "maria.ivanova@example.com",
+//   },
+//   {
+//     name: "Іван",
+//     firstname: "Петров",
+//     age: 33,
+//     email: "ivan.petrov@example.com",
+//   },
+//   {
+//     name: "Катерина",
+//     firstname: "Сидоренко",
+//     age: 45,
+//     email: "katerina.sydorenko@example.com",
+//   },
+//   {
+//     name: "Павло",
+//     firstname: "Кузьменко",
+//     age: 28,
+//     email: "pavlo.kuzmenko@example.com",
+//   },
+//   {
+//     name: "Анна",
+//     firstname: "Мартиненко",
+//     age: 32,
+//     email: "anna.martynenko@example.com",
+//   },
+//   {
+//     name: "Євген",
+//     firstname: "Тарасов",
+//     age: 36,
+//     email: "yevhen.tarasov@example.com",
+//   },
+//   {
+//     name: "Нікіта",
+//     firstname: "Кравець",
+//     age: 27,
+//     email: "nikita.kravets@example.com",
+//   },
+//   {
+//     name: "Ольга",
+//     firstname: "Ільїна",
+//     age: 39,
+//     email: "olga.ilina@example.com",
+//   },
+//   {
+//     name: "Артем",
+//     firstname: "Даниленко",
+//     age: 44,
+//     email: "artem.danilenko@example.com",
+//   },
+//   {
+//     name: "Марина",
+//     firstname: "Ковальова",
+//     age: 31,
+//     email: "marina.kovalova@example.com",
+//   },
+//   {
+//     name: "Ірина",
+//     firstname: "Савченко",
+//     age: 42,
+//     email: "iryna.savchenko@example.com",
+//   },
+//   {
+//     name: "Денис",
+//     firstname: "Федоренко",
+//     age: 30,
+//     email: "denys.fedorenko@example.com",
+//   },
+//   {
+//     name: "Олександр",
+//     firstname: "Марченко",
+//     age: 46,
+//     email: "oleksandr.marchenko@example.com",
+//   },
+//   {
+//     name: "Валерія",
+//     firstname: "Кравчук",
+//     age: 28,
+//     email: "valeriya.kravchuk@example.com",
+//   },
+//   {
+//     name: "Дмитро",
+//     firstname: "Ковальчук",
+//     age: 22,
+//     email: "dmitro.kovalchuk@example.com",
+//   },
+//   {
+//     name: "Олексій",
+//     firstname: "Зайцев",
+//     age: 58,
+//     email: "oleksiy.zaytsev@example.com",
+//   },
+//   {
+//     name: "Софія",
+//     firstname: "Шевченко",
+//     age: 29,
+//     email: "sofiya.shevchenko@example.com",
+//   },
+//   {
+//     name: "Андрій",
+//     firstname: "Кравченко",
+//     age: 41,
+//     email: "andriy.kravchenko@example.com",
+//   },
+//   {
+//     name: "Тетяна",
+//     firstname: "Мельник",
+//     age: 64,
+//     email: "tetiana.melnyk@example.com",
+//   },
+//   {
+//     name: "Олена",
+//     firstname: "Шевчук",
+//     age: 34,
+//     email: "olena.shevchuk@example.com",
+//   },
+//   {
+//     name: "Юрій",
+//     firstname: "Коваленко",
+//     age: 55,
+//     email: "yuriy.kovalenko@example.com",
+//   },
+//   {
+//     name: "Надія",
+//     firstname: "Бойко",
+//     age: 49,
+//     email: "nadiya.boyko@example.com",
+//   },
+// ];
+
+const data = [
+  ["John", "Doe", 30, "johndoe@example.com"],
+  ["Jane", "Doe", 25, "janedoe@example.com"],
+  ["Bob", "Smith", 45, "bobsmith@example.com"],
+  ["Alice", "Johnson", 27, "alicejohnson@example.com"],
+];
+
 export const App = () => {
   return (
     <Routes>
@@ -19,7 +161,10 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
         <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<ProductDetails />} />
+        <Route
+          path="products/:id"
+          element={<ProductDetails data={data} />}
+        />
       </Route>
     </Routes>
   );
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,25 +1,180 @@
 import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+
+import css from "./ProductDetails.module.css";
+
 import { getProductById } from "../fakeAPI";
 
-export const ProductDetails = () => {
+export const ProductDetails = ({ data }) => {
+  console.log(data);
+  const [tableData, setTableData] = useState(data);
   const { id } = useParams();
   const product = getProductById(id);
+  const handleCellChange = (
+    rowIndex,
+    columnIndex,
+    value
+  ) => {
+    // Створюємо новий масив для даних таблиці зі зміненим значенням комірки
+    const newData = [...tableData];
+    newData[rowIndex][columnIndex] = value;
+    setTableData(newData);
+  };
+
   return (
     <main>
-      <img src="https://via.placeholder.com/960x240" alt="" />
+      <img
+        src="https://via.placeholder.com/960x240"
+        alt=""
+      />
       <div>
         <h2>
           Product - {product.name} - {id}
         </h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
-          sunt excepturi nesciunt iusto dignissimos assumenda ab quae cupiditate
-          a, sed reprehenderit? Deleniti optio quasi, amet natus reiciendis
-          atque fuga dolore? Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Impedit suscipit quisquam incidunt commodi fugiat aliquam
-          praesentium ipsum quos unde voluptatum?
-        </p>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Ім'я</th>
+              <th>Прізвище</th>
+              <th>Вік</th>
+              <th>Електронна пошта</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((cell, columnIndex) => (
+                  <td
+                    key={columnIndex}
+                    contentEditable
+                    onBlur={(event) =>
+                      handleCellChange(
+                        rowIndex,
+                        columnIndex,
+                        event.target.innerText
+                      )
+                    }
+                  >
+                    {cell}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
   );
 };
+
+{
+  /* <table>
+          <tr>
+            <td>Номер картки</td>
+            <td>ПІБ</td>
+            <td></td>
+            <td>Створити картку</td>
+            <td>Добавити до існуючої</td>
+            <td></td>
+            <td> </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Іванов Іван Іванович</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td> </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Майно</td>
+            <td></td>
+            <td></td>
+            <td>Дата та номер приказу зарахування</td>
+            <td>Зріст</td>
+            <td></td>
+            <td>180 </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Кашкет польовий (кепі бойове)</td>
+            <td></td>
+            <td>1</td>
+            <td></td>
+            <td>Обєм грудей</td>
+            <td></td>
+            <td>105 </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            
+            <td>Шапка феска (шапка підшоломник)</td>
+            <td></td>
+            <td>1</td>
+            <td>1 від 24.02.22</td>
+            <td>Розмір </td>
+            <td>голови</td>
+            <td>57 </td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>взуття</td>
+            <td>43 </td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table> */
+}
+{
+  /* <table id="myTable">
+          <thead>
+            <tr>
+              <th>Ім'я</th>
+              <th>Прізвище</th>
+              <th>Вік</th>
+              <th>Електронна пошта</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td contentEditable="true">Іван</td>
+              <td contentEditable="true">Іванов</td>
+              <td contentEditable="true">25</td>
+              <td contentEditable="true">
+                ivanov@example.com
+              </td>
+            </tr>
+            <tr>
+              <td contentEditable="true">Петро</td>
+              <td contentEditable="true">Петров</td>
+              <td contentEditable="true">30</td>
+              <td contentEditable="true">
+                petrov@example.com
+              </td>
+            </tr>
+            <tr>
+              <td contentEditable="true">Марія</td>
+              <td contentEditable="true">Шевченко</td>
+              <td contentEditable="true">28</td>
+              <td contentEditable="true">
+                shevchenko@example.com
+              </td>
+            </tr>
+          </tbody>
+        </table> */
+}
